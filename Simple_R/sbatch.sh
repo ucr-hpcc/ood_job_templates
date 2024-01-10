490d790db81e3ae818d55ba2b729d49f2d798a3f
root <root@ondemand.web.hpcc.ucr.edu>
2024-01-10 13:43:33 -0800

    Initial Commit

---
+++ Simple_R/sbatch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10G
+#SBATCH --time=1-00:00:00
+
+# The latest R is loaded by default
+# However, if you want to use a diferent version, then do so here
+module load R
+
+# Use Rscript to run R script
+Rscript myRscript.R
---
+++ default/sbatch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#SBATCH -p short
+#SBATCH -t 2:00:00
+#SBATCH -c 1
+#SBATCH --mem 1GB
+
+echo "Hello World!"
