64f605ad9bb096e3b17b4a61db20505d4e99b2fb
Emerson Jacobson <emwjacobson@gmail.com>
2024-01-10 13:52:30 -0800

    Added basic python example

---
+++ Simple_Python/myscript.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+answer = (7*3)*2
+
+print("Hello World!")
+print(F"The answer is {answer}")
+
---
+++ Simple_Python/sbatch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10G
+#SBATCH --time=1-00:00:00
+
+which python
+python myscript.py
+
