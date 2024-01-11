5fb77faea47663d58cc06569f9677e651da6a176
Emerson Jacobson <emwjacobson@gmail.com>
2024-01-11 09:37:18 -0800

    Slight tweak in default sbatch

--- default/sbatch.sh
+++ default/sbatch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH -p short          # Which partition to use. Options are: short, epyc, intel, batch
-#SBATCH -t 2:00:00        # Time for the job to run
-#SBATCH --cpus-per-task 1 # Number of cores to request, per task
-#SBATCH --mem 1GB         # Amount of memory to request
-#SBATCH --nodes=1         # Number of nodes to use for MPI Jobs
-#SBATCH --ntasks=1        # Number of tasks to use for MPI Jobs
+#SBATCH -p short           # Which partition to use. Options are: short, epyc, intel, batch, highmem, gpu
+#SBATCH -t 2:00:00         # Time for the job to run
+#SBATCH --cpus-per-task 1  # Number of cores to request, per task
+#SBATCH --mem 1G           # Amount of memory to request
+#SBATCH --nodes=1          # Number of nodes to use for MPI Jobs
+#SBATCH --ntasks=1         # Number of tasks to use for MPI Jobs
 #SBATCH --mail-user=useremail@address.com
 #SBATCH --mail-type=ALL
 #SBATCH --job-name="my test job"
