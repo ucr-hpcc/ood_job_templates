8797361dbdb2c7d73b629408c4a23ca1ceb1ebfc
Emerson Jacobson <emwjacobson@gmail.com>
2024-01-11 09:30:44 -0800

    Added more option to default job

--- default/sbatch.sh
+++ default/sbatch.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-#SBATCH -p short
-#SBATCH -t 2:00:00
-#SBATCH -c 1
-#SBATCH --mem 1GB
+#SBATCH -p short          # Which partition to use. Options are: short, epyc, intel, batch
+#SBATCH -t 2:00:00        # Time for the job to run
+#SBATCH --cpus-per-task 1 # Number of cores to request, per task
+#SBATCH --mem 1GB         # Amount of memory to request
+#SBATCH --nodes=1         # Number of nodes to use for MPI Jobs
+#SBATCH --ntasks=1        # Number of tasks to use for MPI Jobs
+#SBATCH --mail-user=useremail@address.com
+#SBATCH --mail-type=ALL
+#SBATCH --job-name="my test job"
 
-echo "Hello World!"
+date
+
+MACHINE=$(hostname)
+
+echo "Hello World! I am running on ${MACHINE}"
+
